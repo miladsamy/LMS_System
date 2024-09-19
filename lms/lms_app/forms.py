82afdf48cc11cf4dcf8431834fad2d66f4b18c65
miladsamy <miladsamy04@gmail.com>
2024-09-20 02:00:56 +0300

    this is my LMS project

---
+++ lms/lms_app/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Book,Category
+
+class BookForm(forms.ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title','author','photo','photo_author','pages','price','retal_price_day',
+                  'retal_period','total_retal','status','category',
+                  ]
+        widgets = {
+            'title': forms.TextInput(attrs={'class':'form-control'}),
+            'author': forms.TextInput(attrs={'class':'form-control'}),
+            'photo': forms.FileInput(attrs={'class':'form-control'}),
+            'photo_author': forms.FileInput(attrs={'class':'form-control'}),
+            'pages': forms.NumberInput(attrs={'class':'form-control'}),
+            'price': forms.NumberInput(attrs={'class':'form-control'}),
+            'retal_price_day': forms.NumberInput(attrs={'class':'form-control','id':'retalprice'}),
+            'retal_period': forms.NumberInput(attrs={'class':'form-control','id':'retaldays'}),
+            'total_retal': forms.NumberInput(attrs={'class':'form-control','id':'totalretal'}),
+            'status': forms.Select(attrs={'class':'form-control'}),
+            'category': forms.Select(attrs={'class':'form-control'}),
+        }
+
+class CategoryForm(forms.ModelForm):
+    class Meta:
+        model=Category
+        fields= ['name']
+
+        widgets ={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+        }
